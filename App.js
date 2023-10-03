34c43536f2a7e4b77b05c02e9499f4ad3fcc3e15
Mikel <mikel.cruz@ikasle.aeg.eus>
2023-10-03 10:43:37 +0200

    Creacion de import de las librerias

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler'
+
 
 export default function App() {
   return (
