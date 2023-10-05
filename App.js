b905c95e953b522520d44d826d78635e6ee17ac6
Mikel <mikel.cruz@ikasle.aeg.eus>
2023-10-05 09:30:46 +0200

    Correccion de posicionamiento del codigo de NavigationContainer

--- App.js
+++ App.js
@@ -18,38 +18,6 @@ import {MaterialCommunityIcons} from '@expo/vector-icons'
 
 const Tab = createMaterialTopTabNavigator();
 
-<NavigationContainer>
-  <Tab.Navigator
-    swipeEnabled='true'
-    tabBarOptions={{
-      showIcon: true,
-      showLabel: false,
-      activeTintColor: '#3a86e9',
-      inactiveTintColor: '#9F9F9F',
-    }}
-    screenOptions={({ route }) => ({
-      tabBarIcon: ({ focused, color, size }) =>{
-      let iconName;
-      if(route.name === 'Home') iconName = 'home'
-      else if (route.name === 'Friends') iconName = 'account-multiple-outline'
-      else if (route.name === 'Groups') iconName = 'account-group'
-      else if (route.name === 'Profile') iconName = 'account-circle-outline'
-      else if (route.name === 'Notifications') iconName = 'bell-outline'
-      else if (route.name === 'Menu') iconName = 'menu'
-      return <MaterialCommunityIcons name={iconName} size={26} color={color} />;
-    },
-    })}
-    >
-      <Tab.Screen name = "Home"           component={Home} />
-      <Tab.Screen name = "Friends"        component={Friends} />
-      <Tab.Screen name = "Groups"         component={Groups} />
-      <Tab.Screen name = "Profile"        component={Profile} />
-      <Tab.Screen name = "Notifications"  component={Notifications} />
-      <Tab.Screen name = "Menu"           component={Menu} />
-  </Tab.Navigator>
-</NavigationContainer>
-
-
 
 const App = () => {
   return (
@@ -60,6 +28,36 @@ const App = () => {
     />
     <ScrollView>
       <Header />
+        <NavigationContainer>
+            <Tab.Navigator
+              swipeEnabled='true'
+              tabBarOptions={{
+                showIcon: true,
+                showLabel: false,
+                activeTintColor: '#3a86e9',
+                inactiveTintColor: '#9F9F9F',
+              }}
+              screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, color, size }) =>{
+                let iconName;
+                if (route.name === 'Home') iconName = 'home'
+                else if (route.name === 'Friends') iconName = 'account-multiple-outline'
+                else if (route.name === 'Groups') iconName = 'account-group'
+                else if (route.name === 'Profile') iconName = 'account-circle-outline'
+                else if (route.name === 'Notifications') iconName = 'bell-outline'
+                else if (route.name === 'Menu') iconName = 'menu'
+                return <MaterialCommunityIcons name={iconName} size={26} color={color} />;
+              },
+              })}
+              >
+                <Tab.Screen name = "Home"           component={Home} />
+                <Tab.Screen name = "Friends"        component={Friends} />
+                <Tab.Screen name = "Groups"         component={Groups} />
+                <Tab.Screen name = "Profile"        component={Profile} />
+                <Tab.Screen name = "Notifications"  component={Notifications} />
+                <Tab.Screen name = "Menu"           component={Menu} />
+            </Tab.Navigator>
+          </NavigationContainer>
     </ScrollView>
     </>
   );
